ada586a6ca796851bc31396a24fd9dc5feaccf20
abhishek malvadkar ranoji <abhishek@dev.com>
2023-02-17 18:40:03 +0530

    Added JSX HTML TAG extension in visual studio code

---
+++ react-practice-shoot/learning.md
@@ -0,0 +1,3 @@
+# My Learning
+
+### Install JSX html tag extension in your visual studio code so you will get suggestion of html tag in JSX code
--- react-practice-shoot/src/App.js
+++ react-practice-shoot/src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 
 function App() {
   return (
+    // JSX code START
     <div>
       <h1>Let's Learn React JS</h1>
     </div>
+    // JSX code END
   );
 }
 

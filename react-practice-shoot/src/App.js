d458e83a19a4e38660a7932ed44c60633d72c117
abhishek malvadkar ranoji <abhishek@dev.com>
2023-02-18 18:39:18 +0530

    Refactored code

--- react-practice-shoot/src/App.js
+++ react-practice-shoot/src/App.js
@@ -5,9 +5,7 @@ function App() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users") // Will give you promise
-      .then((response) => response.json()) // Will give you promise , response.json() will give body as promise , only response will give you body , headers , status etc...
-      .then((users) => setUsers(users));
+    fetchUsers();
   }, []);
 
   console.log("users value", users);
@@ -19,6 +17,12 @@ function App() {
     </div>
     // JSX code END
   );
+
+  function fetchUsers() {
+    fetch("https://jsonplaceholder.typicode.com/users") // Will give you promise
+      .then((response) => response.json()) // Will give you promise , response.json() will give body as promise , only response will give you body , headers , status etc...
+      .then((users) => setUsers(users));
+  }
 }
 
 export default App;

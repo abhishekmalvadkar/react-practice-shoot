1f16983a9c61ca011749cde849ccf148e9a2f87a
abhishek malvadkar ranoji <abhishek@dev.com>
2023-02-18 18:34:19 +0530

    Bug fix to preveny calling fetch api code every time when users state changes using useEffect hook

--- react-practice-shoot/src/App.js
+++ react-practice-shoot/src/App.js
@@ -4,9 +4,11 @@ import "./App.css";
 function App() {
   const [users, setUsers] = useState([]);
 
-  fetch("https://jsonplaceholder.typicode.com/users") // Will give you promise
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users") // Will give you promise
       .then((response) => response.json()) // Will give you promise , response.json() will give body as promise , only response will give you body , headers , status etc...
       .then((users) => setUsers(users));
+  }, []);
 
   console.log("users value", users);
 

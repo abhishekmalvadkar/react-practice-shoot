b2f963063ab2dd32cc6173eb44d79e16b0842715
abhishek malvadkar ranoji <abhishek@dev.com>
2023-02-18 18:23:53 +0530

    How to call rest api from react using fetch api code

--- react-practice-shoot/src/App.js
+++ react-practice-shoot/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 
 function App() {
+
+  fetch('https://jsonplaceholder.typicode.com/users') // Will give you promise
+  .then((response) => response.json()) // Will give you promise , response.json() will give body as promise , only response will give you body , headers , status etc...
+  .then((body)=> console.log(body));
+
   return (
     // JSX code START
     <div>

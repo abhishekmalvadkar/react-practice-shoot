8ec0a0048d30fb36f7445737a4edfd69ad9bd327
abhishek malvadkar ranoji <abhishek@dev.com>
2023-02-18 19:06:05 +0530

    Created book mark api spring boot project

---
+++ source/bookmark-api/src/test/java/in/bookmark/api/bookmarkapi/BookmarkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.bookmark.api.bookmarkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

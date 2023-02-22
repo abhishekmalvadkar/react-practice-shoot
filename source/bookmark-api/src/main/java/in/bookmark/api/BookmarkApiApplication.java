d038d79606108b875b7ad695153542587e90063a
abhishek malvadkar ranoji <abhishek@dev.com>
2023-02-22 20:16:51 +0530

    logging best practce to improve performanace

--- source/bookmark-api/src/main/java/in/bookmark/api/BookmarkApiApplication.java
+++ source/bookmark-api/src/main/java/in/bookmark/api/BookmarkApiApplication.java
@@ -14,8 +14,16 @@ public class BookmarkApiApplication {
 		/*
 		 * TRACE < DEBUG < INFO < WARN  < ERROR
 		 */
-		log.trace("TRACE");
-		log.debug("DEBUG");
+		
+		/**
+		 * In both TRACE and debug log level we are printing verbose info where mentioned function will be called wheather that log level is enabled or not that's why to improve performance we are plcaing this chacek over trace and debug log level
+		 */
+		if (log.isDebugEnabled()) {
+			log.debug("variable name :: {}" , "callingFunction()"); // callingFunction() will execute if debug is enabled or not if we don't write debug enabled if check it
+		}
+		if (log.isTraceEnabled()) {
+			log.trace("variable name :: {}" , "callingFunction()"); // callingFunction() will execute if trace is enabled or not if don't write trace enabled if check it
+		}
 		log.warn("WARN");
 		log.info("INFO");
 		log.error("ERROR");

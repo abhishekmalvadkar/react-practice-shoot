c49de35807c5f820290c608db85a1ae872f689b1
abhishek malvadkar ranoji <abhishek@dev.com>
2023-02-22 20:33:43 +0530

    performance issue in logging

--- source/bookmark-api/src/main/java/in/bookmark/api/BookmarkApiApplication.java
+++ source/bookmark-api/src/main/java/in/bookmark/api/BookmarkApiApplication.java
@@ -18,15 +18,22 @@ public class BookmarkApiApplication {
 		/**
 		 * In both TRACE and debug log level we are printing verbose info where mentioned function will be called wheather that log level is enabled or not that's why to improve performance we are plcaing this chacek over trace and debug log level
 		 */
-		if (log.isDebugEnabled()) {
-			log.debug("variable name :: {}" , "callingFunction()"); // callingFunction() will execute if debug is enabled or not if we don't write debug enabled if check it
-		}
-		if (log.isTraceEnabled()) {
-			log.trace("variable name :: {}" , "callingFunction()"); // callingFunction() will execute if trace is enabled or not if don't write trace enabled if check it
-		}
-		log.warn("WARN");
+		log.debug("name :: {}" , myName()); // myName() will execute if debug is enabled or not if we don't write debug enabled if check it
+		log.trace("age name :: {}" , myAge()); // myName() will execute if trace is enabled or not if don't write trace enabled if check it
+		log.warn("WARN"); 
 		log.info("INFO");
 		log.error("ERROR");
+	}
+	
+	public static String myName() {
+		log.info("Inside myName");
+		return "abhi";
+	}
+	
+	
+	public static int myAge() {
+		log.info("Inside myAge");
+		return 12;
 	}
 
 }

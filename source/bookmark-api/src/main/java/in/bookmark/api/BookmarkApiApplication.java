60cae56726d69216dd1f64015af6f7d67f521d45
abhishek malvadkar ranoji <abhishek@dev.com>
2023-02-22 19:23:18 +0530

    Implementing basic logging in application

--- source/bookmark-api/src/main/java/in/bookmark/api/BookmarkApiApplication.java
+++ source/bookmark-api/src/main/java/in/bookmark/api/BookmarkApiApplication.java
@@ -3,11 +3,22 @@ package in.bookmark.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class BookmarkApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookmarkApiApplication.class, args);
+		/*
+		 * TRACE < DEBUG < INFO < WARN  < ERROR
+		 */
+		log.trace("TRACE");
+		log.debug("DEBUG");
+		log.warn("WARN");
+		log.info("INFO");
+		log.error("ERROR");
 	}
 
 }
